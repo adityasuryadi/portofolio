2d5c9204970c897499eca1d14963fc2a967df3e6
Aditya Suryadi <adityasuryaditya@gmail.com>
2023-10-03 13:46:22 +0700

    fix

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -14,6 +14,7 @@ import {
   faDocker,
 } from "@fortawesome/free-brands-svg-icons";
 import { faDatabase, faServer } from "@fortawesome/free-solid-svg-icons";
+
 export default function Page() {
   const handleScroll = () => {
     let konten: HTMLElement | null = document.getElementById("konten")!;
